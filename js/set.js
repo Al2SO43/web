4b61b92a189a1546680a90a6d303d20312c21a46
Al2SO43 <158013343+Al2SO43@users.noreply.github.com>
2024-04-30 20:41:10 +0800

    Add files via upload

--- js/set.js
+++ js/set.js
@@ -80,7 +80,7 @@ function getBgImg() {
 }
 
 let bg_img_preinstall = {
-    "type": "6", // 1:默认背景 2:必应每日 3:随机风景 4:随机动漫 5:MC酱动漫 6:本地图片
+    "type": "2", // 1:默认背景 2:必应每日 3:随机风景 4:随机动漫 5:MC酱动漫 6:本地图片
     "2": "https://api.dujin.org/bing/1920.php", // 必应每日
     "3": "https://api.aixiaowai.cn/gqapi/gqapi2.php", // 随机风景
     "4": "https://api.aixiaowai.cn/api/api2.php", // 随机动漫
@@ -97,19 +97,19 @@ function setBgImgInit() {
         case "1":
             $('#bg').attr('src', `./img/background${1 + ~~(Math.random() * 10)}.webp`) //随机默认壁纸
             break;
-        case "2":
+        case "3":
             $('#bg').attr('src', bg_img_preinstall[2]); //必应每日
             break;
-        case "3":
+        case "4":
             $('#bg').attr('src', bg_img_preinstall[3]); //随机风景
             break;
-        case "4":
+        case "5":
             $('#bg').attr('src', bg_img_preinstall[4]); //随机动漫
             break;
-        case "5":
+        case "6":
             $('#bg').attr('src', bg_img_preinstall[5]); //MC酱动漫
             break;
-        case "6":
+        case "2":
             $('#bg').attr('src', `./img/bendi/background${1 + ~~(Math.random() * 10)}.jpg`) //本地壁纸
             break
     }
