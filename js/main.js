616438a2aad91f4d3ede029d36d72fcfd73227c6
Al2SO43 <158013343+Al2SO43@users.noreply.github.com>
2024-05-12 10:52:41 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -278,7 +278,7 @@ for (let day of days) {
                 iziToast.show({
                     timeout: 14000,
                     icon: "fa-solid fa-clock",
-                    message: '今天是一个特殊的日子,本站已切换为黑白模式!<br>请前往<a href="https://www.lssjt.com/">『历史上的今天』</a>了解更多信息!'
+                    message: '今天是一个特殊的日子,本站锁定为黑白主题!<br>请前往<a href="https://www.lssjt.com/">『历史上的今天』</a>了解更多信息!'
                 });
             }, 3800);
         }, false);
